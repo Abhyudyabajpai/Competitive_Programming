716979b98d9096b413b58cffbc6e5499e67f5bdf
Abhyudya Bajpai <42513946+Abhyudyabajpai@users.noreply.github.com>
2020-09-10 23:28:56 +0530

    decimal to binary conversion

--- Decimal_to_binary.c
+++ Decimal_to_binary.c
@@ -3,14 +3,14 @@
 int main(){  
 int a[10],n,i;    
 system ("cls");  
-printf("Enter the number to convert: ");    
+printf("Decimal value: ");    
 scanf("%d",&n);    
 for(i=0;n>0;i++)    
 {    
 a[i]=n%2;    
 n=n/2;    
 }    
-printf("\nBinary of Given Number is=");    
+printf("\n Equivalent Binary: ");    
 for(i=i-1;i>=0;i--)    
 {    
 printf("%d",a[i]);    
